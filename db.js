e152166bb5e2a62e33319ffb7ed0b6f21ef6d78f
yasmataz <yasamin.ahmadzadeh@yahoo.com>
2021-03-24 20:21:16 -0400

    Lab5 - Working backend login

---
+++ db.js
@@ -0,0 +1,87 @@
+console.log("hello there");
+var { MongoClient } = require("mongodb");
+var bcrypt = require("bcrypt"); //for password auth
+
+var db = null;
+async function connect() {
+    if (db == null) {
+        var url = "mongodb+srv://dbUser:dbPassword@cluster0.rdapr.mongodb.net/cps888?retryWrites=true&w=majority";
+        var options = {useUnifiedTopology: true};
+
+        var connection = await MongoClient.connect(url, options);
+        connection.db("cps888");
+
+        db = await connection.db("cps888");
+    }
+
+    return db;
+}
+
+async function register(username, password) {
+    var conn = await connect(); // establish connection with database
+    var exsitingUser = await conn.collection('users').findOne({ username }); // pull from collection
+
+    if (exsitingUser != null) {
+        throw new Error('User Exists!')
+    }
+
+    var SALT_ROUNDS = 10; // recomended value for hashing
+    var passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+
+    await conn.collection('users').insertOne({ username, passwordHash })
+}
+
+async function login(username, password) {
+    var conn = await connect();
+    var user = await conn.collection('users').findOne({ username });
+
+    if (user == null) {
+        throw new Error('User does not exsist')
+    }
+
+    var valid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!valid) { 
+        throw new Error("Invalid Password");
+    }
+
+    console.log("Login Successful!")
+}
+
+async function addListItem(username, item) {
+    var conn = await connect();
+
+    await conn.collection('users').updateOne(
+        { username },
+        {
+            $push: {
+                list: item,
+            }
+        }
+    )
+}
+
+async function getListItems(username) {
+    var conn = await connect();
+    var user = await conn.collection('users').findOne({ username });
+
+    console.log("List items:", user.list);
+}
+
+async function deleteListItems(username, item) {
+    var conn = await connect();
+    await conn.collection('users').updateOne(
+       { username },
+       {
+           $pull: {
+               list: item,
+           }
+       } 
+    )
+}
+
+// register('marko', 'pass');
+// login('marko', 'pass')
+addListItem("marko", "test Item")
+// deleteListItems("marko", "dummy item");
+// getListItems("marko");
